829aaed708a3650d6e2137d9359abdb14fa767a0
acmp1 <clarissamirandap@gmail.com>
2016-08-27 15:27:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
+    $(window).resize(function(){
+        if ($(window).width() <= 780) {
+            $("#imagen").height(84.8);
+            $("#imagen").width(220.8);
+        }
+    });
 });
