a5023f24f404febb88c9cb5c333e1582d8475b8d
acmp1 <clarissamirandap@gmail.com>
2016-08-28 11:31:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ function initMap(){
             window.location.href='Cuadro.html';
             for(var conta=0;conta<arrServicios[0].numPlatillos;conta++)
             {
-                $("#platillos").append("<div class="formato"><p>"+arrServicios[0].platillos[conta].nombrePlatillo+"  Precio: $"+arrServicios[0].platillos[conta].precio+"</p></div>")
+                $("#platillos").append("<div class="formato"><p>"+arrServicios[0].platillos[conta].nombrePlatillo+"  Precio: $"+arrServicios[0].platillos[conta].precio+"</p></div>");
             }
         });
         
